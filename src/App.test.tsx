1b11851a67b798e6825c6a4e53cdf0aaea525541
Jiin Kim <jiinkim@macbookpros-MacBook-Pro.local>
2021-03-06 22:28:01 -0800

    added function testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,52 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import React from 'react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App';
 
-test('renders without error', () => {
+// unit testing
+test('button has correct initial color', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+
+    // find n element with a role of button and text of 'Change to blue'
+    const colorButton = screen.getByRole('button', { name: 'Change to Blue' });
+
+    // epect the background color to be red 
+    expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+}); 
+ 
+// functional testing
+test('button turns blue when clicked', () => {
+    render(<App />);
+    const colorButton = screen.getByRole('button', { name: 'Change to Blue'});
+    fireEvent.click(colorButton);
+    expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+    expect(colorButton.textContent).toBe('Change to Red'); 
+});
+
+test('initial conditions', () => {
+    render(<App />);
+    // check that the button starts out enabled
+    const colorButton = screen.getByRole('button', { name: 'Change to Blue' });
+    expect(colorButton).toBeEnabled();
+    // check that the checkbox starts out unchecked
+    const checkbox = screen.getByRole('checkbox', { name: 'Disable button' });
+    expect(checkbox).not.toBeChecked();
+
+    fireEvent.click(colorButton);
+    fireEvent.click(checkbox);
+    expect(colorButton).toHaveStyle('background-color: gray');
+    fireEvent.click(checkbox);
+    expect(colorButton).toHaveStyle('background-color: blue');
+});
+
+describe('spaces before camel-case capital letters', () => {
+    test('Works for no inner capital letters', () => {
+        expect(replaceCamelWithSpaces('Red')).toBe('Red');
+    });
+    test('Works for one inner capital letter', () => {
+        expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+    });
+    test('Works for multiple inner capital letters', () => {
+        expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
-const App: FunctionComponent = () => (
-  <div>
-    <h2>Learn React</h2>
-  </div>
-)
+export function replaceCamelWithSpaces(colorName: string) {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1');
+} 
+
+const App: FunctionComponent = () => {
+  const [buttonColor, setButtonColor] = useState<string>('red');
+  const [disabled, setDisabled] = useState<boolean>(false);
+
+  return (
+    <div>
+      <button 
+        style={{backgroundColor: disabled ? 'gray' : buttonColor}} 
+        onClick={() => setButtonColor('blue')}
+        disabled={disabled}
+      >Change to {buttonColor === 'red' ? 'Blue' : 'Red'}</button>
+      <input 
+        type="checkbox"
+        id="disable-button-checkbox"
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
+    </div> 
+  )
+}
 
 export default App;
