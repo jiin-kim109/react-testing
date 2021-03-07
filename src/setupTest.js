d4f912862360c8b551313767fe97d73f72247bbc
Jiin Kim <jiinkim@macbookpros-MacBook-Pro.local>
2021-03-07 14:42:08 -0800

    Added mock service worker

---
+++ src/setupTest.js
@@ -0,0 +1,10 @@
+
+import '@testing-library/jest-dom';
+
+import { server } from './mocks/server';
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
